e7c0a22a5bc6e18e15481e48b3ab0360aa7609e6
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-05-12 10:45:24 +0800

    Add error message for incomplete args

--- laracasts.py
+++ laracasts.py
@@ -1,69 +1,73 @@
-import os
-import mechanize
-
-import urllib2
-import cookielib
-import cgi
-
 import sys
 
-print 'Logging in...'
-
-cj = cookielib.CookieJar()
-br = mechanize.Browser()
-br.set_handle_robots(False)
-
-br.set_cookiejar(cj)
-br.open("https://laracasts.com/login")
-
-br.select_form(nr=0)
-br.form['email'] = sys.argv[1]
-br.form['password'] = sys.argv[2]
-br.submit()
-
-response = br.response().read()
-
-if 'You are now logged in!' in response:
-	print 'You are now logged in! Scraping...'
-
-	download_path = os.path.dirname(os.path.abspath(__file__))
-	download_url = "https://laracasts.com/downloads/%s?type=episode"
-
-	i = int(sys.argv[3]) if len(sys.argv) > 3 else 0
-
-	while True:
-		video_link = download_url%i
-
-		episode_number = '%04d'%i
-
-		print 'Downloading from %s...'%video_link
-
-		br.open(video_link)
-
-		if br.viewing_html():
-			print '%s does not exist.'%episode_number
-		else:
-			response = br.response()
-
-			headers = response.info().headers
-
-			_, params = cgi.parse_header(
-				[
-					header for header in headers
-						if 'Content-Disposition' in header
-				][0])
-
-			title = params['filename']
-			body = response.read()
-
-			filename = 'laracasts/%s %s'%(episode_number, title)
-
-			print 'Saving to %s...'%filename
-
-			f = open(os.path.join(download_path, filename), 'w')
-			f.write(body)
-			f.close()
-
-		i += 1
+if len(sys.argv) < 3:
+	print 'Incomplete number of args.'
+	print 'Usage: python laracasts.py username password [from_episode]'
 else:
-	print 'Failed to log in.'
+	import os
+	import mechanize
+
+	import urllib2
+	import cookielib
+	import cgi
+
+	print 'Logging in...'
+
+	cj = cookielib.CookieJar()
+	br = mechanize.Browser()
+	br.set_handle_robots(False)
+
+	br.set_cookiejar(cj)
+	br.open("https://laracasts.com/login")
+
+	br.select_form(nr=0)
+	br.form['email'] = sys.argv[1]
+	br.form['password'] = sys.argv[2]
+	br.submit()
+
+	response = br.response().read()
+
+	if 'You are now logged in!' in response:
+		print 'You are now logged in! Scraping...'
+
+		download_path = os.path.dirname(os.path.abspath(__file__))
+		download_url = "https://laracasts.com/downloads/%s?type=episode"
+
+		i = int(sys.argv[3]) if len(sys.argv) > 3 else 0
+
+		while True:
+			video_link = download_url%i
+
+			episode_number = '%04d'%i
+
+			print 'Downloading from %s...'%video_link
+
+			br.open(video_link)
+
+			if br.viewing_html():
+				print '%s does not exist.'%episode_number
+			else:
+				response = br.response()
+
+				headers = response.info().headers
+
+				_, params = cgi.parse_header(
+					[
+						header for header in headers
+							if 'Content-Disposition' in header
+					][0])
+
+				title = params['filename']
+				body = response.read()
+
+				filename = 'laracasts/%s %s'%(episode_number, title)
+
+				print 'Saving to %s...'%filename
+
+				f = open(os.path.join(download_path, filename), 'w')
+				f.write(body)
+				f.close()
+
+			i += 1
+	else:
+		print 'Failed to log in.'
