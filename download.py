a144bc59e8cd6cd677dbbc64942fed1f368946d4
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-05-25 13:28:27 +0800

    Reformat download.py to follow PEP8

--- download.py
+++ download.py
@@ -1,47 +1,49 @@
 import cgi
 
+
 def read_in_chunks(file_object):
-	progress = 0
+    progress = 0
 
-	while True:
-		chunk = []
-		for i in range(10):
-			data = file_object.readline()
+    while True:
+        chunk = []
+        for i in range(10):
+            data = file_object.readline()
 
-			if not data:
-				break
+            if not data:
+                break
 
-			chunk.append(data)
+            chunk.append(data)
 
-		if not chunk:
-			break
+        if not chunk:
+            break
 
-		chunk = ''.join(chunk)
-		progress += len(chunk)
+        chunk = ''.join(chunk)
+        progress += len(chunk)
 
-		yield progress, chunk
+        yield progress, chunk
+
 
 def download(response):
-	headers = response.info().headers
+    headers = response.info().headers
 
-	_, params = cgi.parse_header(
-		[
-			header for header in headers
-				if 'Content-Disposition' in header
-		][0])
+    _, params = cgi.parse_header(
+        [
+            header for header in headers
+            if 'Content-Disposition' in header
+        ][0])
 
-	bytes_count = float([
-		header for header in headers
-			if 'Content-Length' in header
-	][0].split(' ')[-1])
+    bytes_count = float([
+        header for header in headers
+        if 'Content-Length' in header
+    ][0].split(' ')[-1])
 
-	title = params['filename']
+    title = params['filename']
 
-	body = [];
+    body = []
 
-	print '\r%d / %d [%d%%]'%(0, bytes_count, 0),
-	for (progress, chunk) in read_in_chunks(response):
-		print '\r%d / %d [%d%%]'%(progress, bytes_count, progress*100/bytes_count),
-		body.append(chunk)
+    print '\r%d / %d [%d%%]' % (0, bytes_count, 0),
+    for (progress, chunk) in read_in_chunks(response):
+        print '\r%d / %d [%d%%]' % (progress, bytes_count, progress * 100 / bytes_count),
+        body.append(chunk)
 
-	return title, ''.join(body)
+    return title, ''.join(body)
