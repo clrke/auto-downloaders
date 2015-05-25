ab74468de6a5ecee710f65b31ce0348d552837fa
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-05-25 10:25:09 +0800

    Add options to change parameters for login

--- virtual_browser.py
+++ virtual_browser.py
@@ -29,18 +29,18 @@ class VirtualBrowser(mechanize.Browser):
 
         self.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-    def login(self, email_address):
+    def login(self, email_address, nr=0, form_email='email', form_password='password'):
         if not self.login_page:
             raise VirtualBrowserException('No login page was specified.')
 
         self.open(self.login_page)
 
         print '\n       Log In'
-        self.select_form(nr=0)
+        self.select_form(nr=nr)
 
         print '   Email: %s' % email_address
-        self.form['email'] = email_address
+        self.form[form_email] = email_address
 
-        self.form['password'] = getpass()
+        self.form[form_password] = getpass()
 
         self.submit()
