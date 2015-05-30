34f084ca6ca6605beba237f0a71e2ad28bed8f8a
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-05-30 09:48:03 +0800

    Rename browser.py to tutsplus.py

--- tutsplus.py
+++ tutsplus.py
@@ -1,58 +1,94 @@
+from getpass import getpass
+import traceback
+from time import sleep
 import sys
 
-if len(sys.argv) < 3:
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import Chrome
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+if len(sys.argv) < 4:
     print 'Incomplete number of args.'
-    print 'Usage: python laracasts.py email_address first_episode_url download_folder'
+    print 'Usage: python tutsplus.py email_address first_episode_url download_folder [seconds_to_wait = 10]'
 
     sys.exit()
 
+if len(sys.argv) > 4:
+    seconds_to_wait = float(sys.argv[4])
+else:
+    seconds_to_wait = 10
+
 import os
-from download import download
-from virtual_browser import VirtualBrowser
+from download import download_from_url
 
 download_path = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     'tutsplus/%s' % sys.argv[3]
 )
 
-print 'Initializing virtual browser...'
+username = sys.argv[1]
+password = getpass()
 
-br = VirtualBrowser('http://tutsplus.com/sign_in')
+browser = Chrome()
+try:
+    soup = BeautifulSoup()
 
-print 'Accessing Tuts+...'
+    browser.set_window_size(1120, 520)
 
-email_address = sys.argv[1]
-br.login(email_address, 2, 'session[login]', 'session[password]')
+    browser.get('http://tutsplus.com/sign_in')
 
-print 'Logging in...'
+    login_form = browser.find_element_by_class_name('sign-in')
 
-response = br.response().read()
+    login_form.find_element_by_name('session[login]').send_keys(username)
+    login_form.find_element_by_name('session[password]').send_keys(password)
 
-if 'Sign Out' in response:
-    print '\nYou are now logged in! Scraping...'
+    login_form.find_element_by_tag_name('button').submit()
 
-    def download_urls(first_page):
-        br.open(first_page)
-        response = br.response().read()
-        soup = BeautifulSoup(br.response().read())
+    def video_links(page_link):
+        while page_link:
+            browser.get(page_link)
+            sleep(seconds_to_wait)
 
-        f = open('asdf.html', 'w')
+            lesson_text = browser.find_element_by_class_name('lesson-index__lesson--current') \
+                .find_element_by_class_name('lesson-index__lesson-text')
 
-        f.write(str(soup))
+            page_title = "%s %s" % (lesson_text.find_element_by_class_name('lesson-index__lesson-number').text,
+                                    lesson_text.find_element_by_class_name('lesson-index__lesson-title').text)
 
+            page_video = WebDriverWait(browser, seconds_to_wait).until(
+                expected_conditions.presence_of_element_located(
+                    (By.TAG_NAME, 'video'))).find_element_by_tag_name('source').get_attribute('src')
+
+            yield page_title, page_video
+
+            try:
+                next_a = browser.find_element_by_class_name('lesson-video__next-link')
+            except NoSuchElementException:
+                next_a = None
+            page_link = next_a.get_attribute('href') if next_a else None
+
+    links = list(video_links(sys.argv[2]))
+
+    browser.quit()
+
+    for link in links:
+        title = link[0]
+        video = link[1]
+        body = download_from_url(video)
+
+        print '\nSaving...',
+
+        f = open(os.path.join(download_path, title), 'w')
+        f.write(body)
         f.close()
-        print soup
 
-        src = soup.find('video').find('source')
-        print src
+        print '\rSaved as %s!' % title
 
-        return src
-
-    for download_url in download_urls(sys.argv[2]):
-        print download_url
-
-    print download_url, 'to', download_path
-else:
-    print 'Failed to log in.'
-
-br.close()
+except Exception as e:
+    print traceback.format_exc()
+finally:
+    browser.quit()
